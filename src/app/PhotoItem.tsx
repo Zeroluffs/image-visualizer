1d29ce1e8ba1c70e0b6b3ad07b3f139820e24207
zeroluffs <zeroluffs@gmail.com>
2023-05-24 16:47:31 -0500

    added author information

--- src/app/PhotoItem.tsx
+++ src/app/PhotoItem.tsx
@@ -7,13 +7,28 @@ type Props = {
 };
 export function PhotoItem({ photo }: Props) {
   return (
-    <div className="relative w-[324px] h-[300px] m-auto">
-      <Image
-        src={photo.urls.regular}
-        fill
-        className="object-cover w-full h-full"
-        alt={photo.alt_description || photo.description || photo.id}
-      />
+    <div className="m-auto w-[324px]">
+      <div className="relative w-full h-[300px]">
+        <Image
+          src={photo.urls.regular}
+          fill
+          className="object-cover w-full h-full"
+          alt={photo.alt_description || photo.description || photo.id}
+        />
+      </div>
+      <div className="flex flex-col mt-6 gap-2">
+        <div className="flex flex-row items-center gap-3">
+          <Image
+            src={photo.user.profile_image.small}
+            width={24}
+            height={24}
+            alt={photo.user.name}
+            className="rounded-full flex shrink-0"
+          />
+          <p className="text-sm font-semibold truncate">{photo.user.name}</p>
+        </div>
+        <p>{photo.updated_at}</p>
+      </div>
     </div>
   );
 }
